8c201dec8e0154499f9de4ed07dd47aa38f047ee
Mario Mitic <mariomitic@yahoo.com>
2023-02-06 11:30:23 -0800

    With Button

--- js/Fetch_Simple_CatFacts.js
+++ js/Fetch_Simple_CatFacts.js
@@ -1,15 +1,17 @@
 
+function showFact() {
 
 fetch('https://catfact.ninja/fact')  
 .then(function(response) {
 return response.json();
 })
 .then(function(data){
-   console.log(data);
+   //console.log(data);
     document.getElementById('fetchedText').innerText = data.fact;
 
 })
 
+}
 
 
 
