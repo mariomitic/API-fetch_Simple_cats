6e8fa5ccfa4782fd916307a7280be9a442138b4c
damir <damir.cicic@symphony.is>
2023-02-06 20:51:37 +0100

    Added some bootstrap styling

--- js/Fetch_Simple_CatFacts.js
+++ js/Fetch_Simple_CatFacts.js
@@ -1,14 +1,18 @@
 
+var btn = document.getElementById("refresh-btn")
+btn.addEventListener("click", refreshFunc)
 
-fetch('https://catfact.ninja/fact')  
-.then(function(response) {
-return response.json();
-})
-.then(function(data){
-   console.log(data);
-    document.getElementById('fetchedText').innerText = data.fact;
+function refreshFunc(){
+    fetch('https://catfact.ninja/fact')
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data){
+       console.log(data);
+        document.getElementById('fetchedText').innerText = data.fact;
+    })
+}
 
-})
 
 
 
