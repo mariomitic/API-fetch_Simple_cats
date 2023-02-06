613579fa32f9d6bb7f112fbd3e00cb2aadf9a36c
Mario Mitic <mariomitic@yahoo.com>
2023-02-06 09:31:17 -0800

    first commit

---
+++ js/Fetch_Simple_CatFacts.js
@@ -0,0 +1,21 @@
+
+
+fetch('https://catfact.ninja/fact')  
+.then(function(response) {
+return response.json();
+})
+.then(function(data){
+   console.log(data);
+    document.getElementById('fetchedText').innerText = data.fact;
+
+})
+
+
+
+
+
+
+
+
+
+
